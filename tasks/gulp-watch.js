02ba79553596c81416067ca89931ac335b123c4e
ghpabs <pablodenadai@gmail.com>
2016-03-30 15:37:53 +0100

    feat(gulp): implement gulp assets watcher

--- tasks/gulp-watch.js
+++ tasks/gulp-watch.js
@@ -5,6 +5,7 @@ var gulp = require('gulp');
 var index = require('./gulp-index'),
 	unit = require('./gulp-karma'),
 	scss = require('./gulp-scss'),
+	assets = require('./gulp-assets'),
 	typescript = require('./gulp-typescript');
 
 module.exports = function watch() {
@@ -18,8 +19,14 @@ module.exports = function watch() {
 		'!src/scripts/**/*.spec.ts'
 	], gulp.series(typescript, unit));
 
-	gulp.watch('src/scripts/**/*.spec.ts', unit);
+	gulp.watch([
+		'src/fonts/**/*.{eot,ttf,otf,woff}',
+		'src/images/**/*.{png,jpg,gif}'
+	], gulp.series(assets));
 
 	gulp.watch('src/scss/**/*.scss', scss);
+
 	gulp.watch('src/index.html', index);
+
+	gulp.watch('src/scripts/**/*.spec.ts', unit);
 };
